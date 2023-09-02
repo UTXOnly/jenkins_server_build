da29d756eed40689ee66b9d55c121a7a8bd6eaf0
Brian Hartford <bhartford419@gmail.com>
2023-09-02 13:13:34 -0400

    update jenkinsfile

--- build.sh
+++ build.sh
@@ -7,12 +7,13 @@ sudo apt update -y || { echo "Failed to update package lists"; exit 1; } && sudo
 
 sudo apt install -y openjdk-17-jre || { echo "Failed to install JDK"; exit 1; }
 
+
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
+
 sudo apt-get install jenkins -y
 
 sudo apt update || { echo "Failed to update package lists after adding Jenkins repository"; exit 1; }
